03b1dc2362e276bb31ed4632657367900d835e57
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-09-27 23:38:13 +0200

    items part 1
    only editing fe8 items and adding text for new, still need new icons and anims

--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -1,3 +1,4 @@
 set idsFromTables="%~dp0Tools\IDsFromTables\IDsFromTables.py"
 set enumerate="%~dp0Tools\Enumerate\Enumerate.py"
 %idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event %enumerate% Chapter
+%idsFromTables% Tables\NightmareModules\Items\ItemTable.csv Definitions\Generated\ItemIDs.event %enumerate% Item  --oneIndexed --ignoreFirstItem
