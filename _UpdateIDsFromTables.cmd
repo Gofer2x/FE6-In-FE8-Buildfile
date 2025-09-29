bef4e7b139a818e754d0db360681427859f8f989
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-09-29 21:01:08 +0200

    items classes
    eradicating values with definitions, vanilla and not
    to do next, fill out the fe8 ones that i left blanked for. some  reason

--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -2,3 +2,4 @@ set idsFromTables="%~dp0Tools\IDsFromTables\IDsFromTables.py"
 set enumerate="%~dp0Tools\Enumerate\Enumerate.py"
 %idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event %enumerate% Chapter
 %idsFromTables% Tables\NightmareModules\Items\ItemTable.csv Definitions\Generated\ItemIDs.event %enumerate% Item  --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\CharactersClasses\ClassTable.csv Definitions\Generated\ClassIDs.event %enumerate% Class --oneIndexed --ignoreFirstItem
