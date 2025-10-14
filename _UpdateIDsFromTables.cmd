e322183e25af14aa6f178829132e4f211a1361ac
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-10-14 23:04:13 +0200

    characters

--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -1,5 +1,6 @@
 set idsFromTables="%~dp0Tools\IDsFromTables\IDsFromTables.py"
 set enumerate="%~dp0Tools\Enumerate\Enumerate.py"
-%idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event %enumerate% Chapter
-%idsFromTables% Tables\NightmareModules\Items\ItemTable.csv Definitions\Generated\ItemIDs.event %enumerate% Item  --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\CharactersClasses\CharacterTable.csv Definitions\Generated\CharacterIDs.event %enumerate% Character --oneIndexed
 %idsFromTables% Tables\NightmareModules\CharactersClasses\ClassTable.csv Definitions\Generated\ClassIDs.event %enumerate% Class --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\Items\ItemTable.csv Definitions\Generated\ItemIDs.event %enumerate% Item  --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event %enumerate% Chapter
