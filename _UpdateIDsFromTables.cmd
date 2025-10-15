8643eb80d2719fcc2ad4ba2ab9221cac1983b935
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-10-15 11:59:16 +0200

    wmap world

--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -4,3 +4,5 @@ set enumerate="%~dp0Tools\Enumerate\Enumerate.py"
 %idsFromTables% Tables\NightmareModules\CharactersClasses\ClassTable.csv Definitions\Generated\ClassIDs.event %enumerate% Class --oneIndexed --ignoreFirstItem
 %idsFromTables% Tables\NightmareModules\Items\ItemTable.csv Definitions\Generated\ItemIDs.event %enumerate% Item  --oneIndexed --ignoreFirstItem
 %idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event %enumerate% Chapter
+%idsFromTables% Tables\NightmareModules\WorldMap\Nodes.csv Definitions\Generated\WMNodeIDs.event %enumerate% Node
+%idsFromTables% Tables\NightmareModules\WorldMap\Roads.csv Definitions\Generated\WMRoadIDs.event %enumerate% Road
