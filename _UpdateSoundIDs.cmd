a35968f7f563dfe407c05c7582cec2615a2738dd
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-10-17 22:38:28 +0200

    more accurate name

--- _UpdateSoundIDs.cmd
+++ _UpdateSoundIDs.cmd
@@ -1,6 +1,6 @@
 set csvPath=Tools\SoundScripts\FE6toFE8Sounds.csv
 set updateSoundIDsScriptPath=Tools\SoundScripts\UpdateSoundIDs.py
-set soundIDsPath=Definitions\Generated\SoundIDs.event
+set soundIDsPath=Definitions\Generated\NewFE6SoundIDs.event
 
 cd %~dp0
 
